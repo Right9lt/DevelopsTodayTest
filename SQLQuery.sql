90293503575852cd0a6f2e78e887c34a1a4e67ae
Valentyn Renhach <40607069+Right9lt@users.noreply.github.com>
2025-01-26 08:16:23 +0200

    Add used sqlquery

---
+++ SQLQuery.sql
@@ -0,0 +1,15 @@
+﻿create table Trips (
+tpep_pickup_datetime datetimeoffset,
+tpep_dropoff_datetime datetimeoffset, 
+passenger_count int,
+trip_distance float,
+store_and_fwd_flag varchar(3),
+PULocationID int,
+DOLocationID int,
+fare_amount float,
+tip_amount float,
+);
+
+
+
+
